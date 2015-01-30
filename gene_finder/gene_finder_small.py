f58c336dbb74ec4e6207c0875c8d189ba3457cbf
Byron Wasti <byron.wasti@gmail.com>
2015-01-30 15:52:34 -0500

    Fewest number of characters too!

--- gene_finder/gene_finder_small.py
+++ gene_finder/gene_finder_small.py
@@ -1,5 +1,23 @@
+'''
+This code is actually somewhat readable
+'''
 def find_all_ORFs_both_strands(dna):
-
     return [ idna[idna.find("ATG"):threes] for idna in (dna, ''.join([('A','T','G','C')[ ('A','T','G','C').index(i)+ ( 1 if ('A','T','G','C').index(i)%2 == 0 else -1)] for i in dna[::-1]  ])) for j in range(3) for threes in range(j, len(idna)-1,3) if idna[threes:threes+3] in ("TAG","TAA","TGA") and len(idna[idna.find("ATG"):threes])%3 == 0 and len(idna[idna.find("ATG"):threes]) != 0]
 
+
+
+'''
+This code uses the fewest number of characters for this
+particular algorithm
+'''
+def A(d):
+    return[c[c.find("ATG"):t]for c in (d,''.join([('A','T','G','C')[('A','T','G','C').index(i)+(1 if ('A','T','G','C').index(i)%2==0 else -1)] for i in d[::-1]])) for j in range(3) for t in range(j,len(c)-1,3) if c[t:t+3] in ("TAG","TAA","TGA") and len(c[c.find("ATG"):t])%3==0 and len(c[c.find("ATG"):t])!=0]
+
+
+
+'''
+This is the testing of the functions
+'''
 print find_all_ORFs_both_strands("CTAATGCGAATGTAGCATCAAA")
+print A("CTAATGCGAATGTAGCATCAAA")
+
