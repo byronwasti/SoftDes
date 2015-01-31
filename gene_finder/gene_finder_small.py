b775124ffb09055cc958f0989fdf1387548f182a
Byron Wasti <byron.wasti@gmail.com>
2015-01-31 12:39:09 -0500

    Going back to function structure for now

--- gene_finder/gene_finder_small.py
+++ gene_finder/gene_finder_small.py
@@ -15,6 +15,21 @@ def A(d):return[c[c.find("ATG"):t]for c in (d,''.join([b[b.index(i)+(1 if b.inde
 
 
 #This is the testing of the functions
-print find_all_ORFs_both_strands("CTAATGCGAATGTAGCATCAAA")
-print A("CTAATGCGAATGTAGCATCAAACTAATGCGAATGTAGCATCAAA")
+#print find_all_ORFs_both_strands("CTAATGCGAATGTAGCATCAAA")
+#print A("CTAATGCGAATGTAGCATCAAACTAATGCGAATGTAGCATCAAA")
+
+def find_all_ORFs_both_strands_full(dna):
+    comp = { 'A':'T', 'T':'A', 'G':'C', 'C':'G' }
+    ORFs = []
+    for idna in ( dna, ''.join([comp[i] for i in dna[::-1]])):
+        for j in range(3):
+            for threes in range(j,len(idna)-1,3):
+                if idna[threes:threes+3] in ("TAG","TAA","TGA"):
+                    for k in range(j,threes,3):
+                        if idna[k:k+3] == "ATG":
+                            ORFs.append(idna[k:threes])
+                            break
+    return ORFs
+
+print find_all_ORFs_both_strands_full("CTAATGCGAATGTAGCATCAAACTAATGCGAATGTAGCATCAAACTAATGCGAATGTAGCATCAAA")
 
