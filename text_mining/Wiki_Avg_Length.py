f98552d816afda65cf9d2b9399fb901ad9749dc9
byronwasti <byron.wasti@gmail.com>
2015-02-26 08:59:59 -0500

    Finished comments

--- text_mining/Wiki_Avg_Length.py
+++ text_mining/Wiki_Avg_Length.py
@@ -5,11 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import anydbm
 
+# This module is awesome
 db = anydbm.open('CACHE.db','c')
 
+
 def Avg( s ):
+    ''' The Averaging Function
+        Takes in a string, searches for the Wiki article
+        and then returns the average length of words.
+        No there is no error handling.
+    '''
     length = 0
     numb = 0
+
+    # 
     if s in db:
         return float(db[s])
     article = Wikipedia().search(s)
@@ -17,30 +26,31 @@ def Avg( s ):
         if article.sections[i].title == "See also":
             break
         text = article.sections[i].content.replace('\n',' ')
-        lengthtmp = 0
-        numbtmp = 0
         for words in text.split(' '):
-            length += len(words)
-            lengthtmp += len(words)
             numb += 1
             numbtmp += 1
     db[s] = str( float(length)/float(numb) )
     return float(length)/float(numb)
 
 def Take_AVG( names ):
+    ''' Take average of a list of strings
+        Returns a list of averages for that original list
+    '''
     return [ Avg( an ) for an in names ]
 
 def Plot_Stuff( data, names ):
+    ''' Plots everything
+    '''
     Sep = np.arange(len(data))
     width = .5
     fig, ax = plt.subplots()
     rects = ax.bar(Sep+width/2.0, data, width, color='r')
-
     ax.set_xticks(Sep+width)
     ax.set_xticklabels(names, rotation='vertical')
     ax.set_title('States')
     ax.set_ylabel('Average Word Length')
 
+    # Writes the actual value above the rectangles
     def autolabel(rects):
         for i,rect in enumerate(rects):
             height = data[i] 
@@ -51,6 +61,7 @@ def Plot_Stuff( data, names ):
 
     plt.show()
 
+# The different list of words used in the analysis
 ANIMALS = ['cat', 'dog', 'duck', 'rat', 'ferret','llama','chicken','rooster', 'snake']
 SCHOOLS = ['olin college', 'mit', 'bunker hill community college', 'harvard university', 'princeton']
 COUNTRIES = ['usa','mexico','spain','portugal','sweden','canada','russia']
@@ -60,10 +71,9 @@ ACADEMIC = ['physics','computer science','biology','science','art']
 WORDS_LONG = ['antidisestablishmentarianism', 'pneumonoultramicroscopicsilicovolcanoconiosis', 'lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoiosiraiobaphetraganopterygon','supercalifragilisticexpialidocious', 'pseudopseudohypoparathyroidism', 'honorificabilitudinitatibus']
 WORDS_SHORT = ['sit','art','ace','aim','air','gun','bus','egg','cap','nip','nod']
 
-DATA = STATES
-Plot_Stuff( Take_AVG( DATA ) , DATA )
-
-#db['ANIMALS'] = str(sum(Take_AVG(DATA))/float(len(Take_AVG(DATA))))
-
-#DATA_FULL = ['ANIMALS','SCHOOLS','COUNTRIES','STATES','SPORTS','ACADEMIC','WORDS_LONG','WORDS_SHORT']
-#Plot_Stuff( Take_AVG( DATA_FULL ) , DATA_FULL )
+if __name__ == "__main__":
+    DATA = STATES
+    Plot_Stuff( Take_AVG( DATA ) , DATA )
+    #db['ANIMALS'] = str(sum(Take_AVG(DATA))/float(len(Take_AVG(DATA))))
+    #DATA_FULL = ['ANIMALS','SCHOOLS','COUNTRIES','STATES','SPORTS','ACADEMIC','WORDS_LONG','WORDS_SHORT']
+    #Plot_Stuff( Take_AVG( DATA_FULL ) , DATA_FULL )
