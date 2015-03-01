74f715b322b7bc423edd460508ce73a6e69b86e7
byronwasti <byron.wasti@gmail.com>
2015-03-01 10:44:15 -0500

    Sizes are relative to bounding box

--- toolbox/image_processing/face_detect.py
+++ toolbox/image_processing/face_detect.py
@@ -11,9 +11,9 @@ while(True):
     faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
     for (x,y,w,h) in faces:
         frame[y:y+h,x:x+w,:] = cv2.dilate(frame[y:y+h,x:x+w,:], kernel)
-        cv2.circle(frame,(x + w/4, y + h/3), 10, (0,0,255),-1)
-        cv2.circle(frame,(x + 3*w/4, y + h/3), 10, (0,0,255),-1)
-        cv2.ellipse(frame,(x + w/2, y + 4*h/5),(50,20),0,0,180,255,-1)
+        cv2.circle(frame,(x + w/4, y + h/3), (w+h)/30, (0,0,255),-1)
+        cv2.circle(frame,(x + 3*w/4, y + h/3), (w+h)/20, (0,0,255),-1)
+        cv2.ellipse(frame,(x + w/2, y + 4*h/5),(w/5,h/10),0,0,180,255,-1)
         
     # Display the resulting frame
     cv2.imshow('frame',frame)
